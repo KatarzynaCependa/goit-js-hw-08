e8688ebdf3591f448ccece9a22e2446fc3b76747
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-04-05 18:03:00 +0200

    Adding task no. 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+for (const item of galleryItems) {
+  galleryEl.insertAdjacentHTML(
+    'beforeend',
+    `<li><a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a></li>`
+  );
+}
+
+galleryEl.addEventListener('click', event => {
+  event.preventDefault();
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
