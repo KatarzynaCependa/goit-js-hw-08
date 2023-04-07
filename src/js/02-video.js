6fd5c1e7d266c2853c94bb8c2770d1d4098fdf80
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-04-07 14:56:39 +0200

    Adding task no.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const videoEl = document.getElementById('vimeo-player');
+const player = new Player(videoEl);
+const throttle = require('lodash.throttle');
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
